f6bf2155c7142d8cdc99e71e66572371e5919e2f
Ronak H Shah <rons.shah@gmail.com>
2015-12-18 17:47:19 -0500

    Updated logging info

--- iCallSV/combineVCF.py
+++ iCallSV/combineVCF.py
@@ -1,22 +1,27 @@
 '''
 Created on December 18, 2015
-Description: This module will merge multiple vcf file with same headers
+Description: This module will combine multiple vcf file with same headers
 @author: Ronak H Shah
 '''
 '''
 ::Input::
-vcfFiles : List of VCF Files to merge in list data structure
-mergedVCF: Name of the merged vcf to output
+vcfFiles : List of VCF Files to combine in list data structure
+mergedVCF: Name of the combined vcf to output
 
 ::Output::
 It is a merged vcf file 
 '''
 import sys
 import vcf
-vcf_header = vcf.Reader(filename=vcfFiles[1])
-vcf_output = vcf.Writer(mergedVCF,vcf_header)
-for vcffile in vcfFiles:
-	vcf_reader = vcf.Reader(open(vcffile, 'r'))
-	for each in vcf_reader:
-		vcf_output.write_record(each)
-return()
+import logging
+
+def run(vcfFiles,combinedVCF,verbose):
+	vcf_header = vcf.Reader(filename=vcfFiles[1])
+	vcf_output = vcf.Writer(combinedVCF,vcf_header)
+	for vcffile in vcfFiles:
+		vcf_reader = vcf.Reader(open(vcffile, 'r'))
+		for each in vcf_reader:
+			vcf_output.write_record(each)
+	if(verbose):
+		logging.info("Finished combining vcf files")		
+	return(combinedVCF)
