41a4b6f4f51f2e7a9a7897c5e0716c27a3002721
Ronak H Shah <rons.shah@gmail.com>
2015-12-21 18:26:18 -0500

    Added a module to do some threading

---
+++ iCallSV/launchThreads.py
@@ -0,0 +1,23 @@
+
+'''
+Taken from:
+http://www.tutorialspoint.com/python/python_multithreading.htm
+
+'''
+
+import logging
+import threading
+import time
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print "Starting " + self.name
+        # Get lock to synchronize threads
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+        # Free lock to release next thread
+        threadLock.release()
