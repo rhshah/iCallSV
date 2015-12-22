2b21edd6ae86f12a34a147f0fefac119e56efedb
Ronak H Shah <rons.shah@gmail.com>
2015-12-22 13:39:41 -0500

    DEBUGGING

--- iCallSV/FilterDellyCalls.py
+++ iCallSV/FilterDellyCalls.py
@@ -130,6 +130,7 @@ def run(
 
     hotspotDict = chl.ReadHotSpotFile(hotspotFile)
     blacklist = cbl.ReadBlackListFile(blacklistFile)
+    print os.path.split(os.path.basename(inputVcf)[0]), "\n"
     outputVcf = os.path.split(os.path.basename(inputVcf)[0]) + "_filtered.vcf"
     vcf_reader = vcf.Reader(open(inputVcf, 'r'))
     outputFile = outputDir + "/" + outputVcf
