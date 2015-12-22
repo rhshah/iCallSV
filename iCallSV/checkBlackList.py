45410bd7c52ebcbb4b085e890de01053f5a46545
Ronak H Shah <rons.shah@gmail.com>
2015-12-22 13:48:46 -0500

    DEBUGGING

--- iCallSV/checkBlackList.py
+++ iCallSV/checkBlackList.py
@@ -60,13 +60,13 @@ def CheckIfItIsBlacklisted(chr1, start1, chr2, start2, blacklist, range):
         for entries in blacklist:
             (bchr1, bstart1, bchr2, bstart2)=entries.rstrip().split("\t")
             if(((chr1 == bchr1) and (chr2 == bchr2)) or ((chr1 == bchr2) and (chr2 == bchr1))):
-                if(((start <= int(bstart1 - range)) and (start >= int(bstart1 + range))) 
-                   and ((end <= int(bstart2 - range)) and (end <= int(bstart2 + range)))):
+                if(((start1 <= int(bstart1 - range)) and (start1 >= int(bstart1 + range))) 
+                   and ((start2 <= int(bstart2 - range)) and (start2 <= int(bstart2 + range)))):
                     blacklistTag=True
                 else:
                     blacklistTag=False
-                if(((end <= int(bstart1 - range)) and (end >= int(bstart1 + range))) 
-                   and ((start <= int(bstart2 - range)) and (start <= int(bstart2 + range)))):
+                if(((start2 <= int(bstart1 - range)) and (start2 >= int(bstart1 + range))) 
+                   and ((start1 <= int(bstart2 - range)) and (start1 <= int(bstart2 + range)))):
                     blacklistTag=True
                 else:
                     blacklistTag=False
