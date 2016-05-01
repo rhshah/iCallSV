120ad016052c4bb99866dd1942b84686fd5bcef5
Ronak H Shah <rons.shah@gmail.com>
2016-04-30 20:22:26 -0400

    Updated to use latest version of Delly 0.7.3, it not backward compatible

--- iCallSV/Run_Delly.py
+++ iCallSV/Run_Delly.py
@@ -5,7 +5,8 @@ Description: Runs the delly program on case and control bam file to give its res
 '''
 '''
 ::Inputs::
-delly:Path to delly executables (0.6.1 or above)
+delly:Path to delly executables (0.7.3 or above)
+bcftools:Path to bcftools executables (1.3.1 or above)
 type: What ot run in delly, DEL:Deletion, DUP: Duplication,TRA:Translocation, INV:Inversion
 reference: Reference Genome that was used to align the reads.
 controlBam: Path to control/normal bam file
@@ -17,7 +18,7 @@ excludeRegions: Regions to be excluded for calling structural variation.
 outputdir: directory for the output of delly
 debug: If you just wish to test what we will do
 ::Output::
-VCF file having all the structural variants called
+BCF file having all the structural variants called
 '''
 import os
 import sys
@@ -33,6 +34,7 @@ import logging
 
 def run(
         delly,
+        bcftools,
         analysisType,
         reference,
         controlBam,
@@ -50,11 +52,13 @@ def run(
     day = date.today()
     today = day.isoformat()
     tag = analysisType.lower()
+    outputBcf = outputdir + "/" + caseId + "_" + tag + ".bcf"
     outputVcf = outputdir + "/" + caseId + "_" + tag + ".vcf"
     # Check all input variables
     cp.checkFile(controlBam)
     cp.checkFile(caseBam)
     cp.checkFile(delly)
+    cp.checkFile(bcftools)
     cp.checkFile(reference)
     cp.checkFile(excludeRegions)
     cp.checkDir(outputdir)
@@ -66,7 +70,7 @@ def run(
         logging.info("Run_Delly: ProcessID:%s,Date:%s", myPid, today)
     if(debug):
         cmd = delly + " -t " + analysisType + " -g " + reference + " -x " + excludeRegions + \
-            " -q " + str(mapq) + " -o " + outputVcf + " " + caseBam + " " + controlBam
+            " -q " + str(mapq) + " -o " + outputBcf + " " + caseBam + " " + controlBam
         logging.debug("Run_Delly: Command that will be run %s", cmd)
     else:
         # Check if bam index files are there else make them
@@ -90,8 +94,8 @@ def run(
                     "Run_Delly: Bam Index file is not present and we will make it for %s ",
                     caseBai)
             mbi.MakeIndex(caseBam)
-        cmd = delly + " -t " + analysisType + " -g " + reference + " -x " + excludeRegions + \
-            " -q " + str(mapq) + " -o " + outputVcf + " " + caseBam + " " + controlBam
+        cmd = delly + "call -t " + analysisType + " -g " + reference + " -x " + excludeRegions + \
+            " -q " + str(mapq) + " -o " + outputBcf + " " + caseBam + " " + controlBam
         if(verbose):
             logging.info("Run_Delly: Command that will be run:%s", cmd)
         args = shlex.split(cmd)
@@ -102,9 +106,31 @@ def run(
             end_time = time.time()
             totaltime = str(timedelta(seconds=end_time - start_time))
             if(verbose):
-                logging.info(
-                    "Run_Delly: We have finished running Delly for %s using local machine", caseId)
+                logging.info("Run_Delly: We have finished running Delly for %s using local machine", caseId)
                 logging.info("Run_Delly Duration: %s", totaltime)
+            if(os.path.isfile(outputBcf)):
+                retcode = 1
+            else:
+            cmd = bcftools  + " view "  + outputBcf  + " > " + outputVcf
+            if(verbose):
+                logging.info("Run_Delly_bcf2vcf: Command that will be run:%s", cmd)
+            args = shlex.split(cmd)
+            proc = Popen(args)
+            proc.wait()
+            retcode = proc.returncode
+            if(retcode >= 0):  
+                end_time = time.time()
+                totaltime = str(timedelta(seconds=end_time - start_time))
+                if(verbose):
+                    logging.info(
+                        "Run_Delly_bcf2vcf: We have finished running bcftools for %s using local machine", caseId)
+                    logging.info("Run_Delly_bcf2vcf Duration: %s", totaltime)
+            else:
+                logging.fatal(
+                "Run_Delly_bcf2vcf: bcftools is either still running on local machine or it errored out with return code %d for %s",
+                retcode,
+                caseId)
+                sys.exit(1)
         else:
             logging.fatal(
                 "Run_Delly: Delly is either still running on local machine or it errored out with return code %d for %s",
@@ -113,5 +139,6 @@ def run(
             sys.exit(1)
     return(outputVcf)
 
+
 # Testing the module
-# run("/dmp/resources/dev2/bin/delly", "TRA", "/dmp/data/pubdata/hg-fasta/production/Homo_sapiens_assembly19.fasta", "/dmp/hot/shahr2/IMPACT/Test/SVtest/35462375-N_bc45_IMPACTv5-CLIN-20150050_L000_mrg_cl_aln_srt_MD_IR_BR.bam", "/dmp/hot/shahr2/IMPACT/Test/SVtest/35462375-T_bc44_IMPACTv5-CLIN-20150050_L000_mrg_cl_aln_srt_MD_IR_BR.bam","35462375-T", 20, "/dmp/data/mskdata/sv-files/production/human.hg19.excl.tsv", "/dmp/hot/shahr2/IMPACT/Test/SVtest/", False)
+# run("/dmp/resources/dev2/bin/delly","/dmp/resources/dev2/bin/bcftools", "TRA", "/dmp/data/pubdata/hg-fasta/production/Homo_sapiens_assembly19.fasta", "/dmp/hot/shahr2/IMPACT/Test/SVtest/35462375-N_bc45_IMPACTv5-CLIN-20150050_L000_mrg_cl_aln_srt_MD_IR_BR.bam", "/dmp/hot/shahr2/IMPACT/Test/SVtest/35462375-T_bc44_IMPACTv5-CLIN-20150050_L000_mrg_cl_aln_srt_MD_IR_BR.bam","35462375-T", 20, "/dmp/data/mskdata/sv-files/production/human.hg19.excl.tsv", "/dmp/hot/shahr2/IMPACT/Test/SVtest/", False)
--- iCallSV/launchThreads.py
+++ iCallSV/launchThreads.py
@@ -1,4 +1,14 @@
-
+'''
+Created on December 21, 2015
+Description: This module will be launching functions as threads
+@author: Ronak H Shah
+'''
+'''
+::Inputs::
+threadID: Id for the thread
+name: name for the thread
+counter: a counter to keep track of the thread
+'''
 '''
 Taken from:
 http://www.tutorialspoint.com/python/python_multithreading.htm
@@ -15,7 +25,7 @@ class myThread (threading.Thread):
         self.name = name
         self.counter = counter
     def run(self):
-        print "Starting " + self.name
+        logging.info( "Starting %s" , self.name )
         # Get lock to synchronize threads
         threadLock.acquire()
         print_time(self.name, self.counter, 3)
