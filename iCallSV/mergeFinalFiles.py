a0dbf728c556cf9555334f66b06cd5b7e20c1b95
Ronak H Shah <rons.shah@gmail.com>
2016-06-01 14:04:33 -0400

    Bug Fixes

--- iCallSV/mergeFinalFiles.py
+++ iCallSV/mergeFinalFiles.py
@@ -285,9 +285,9 @@ def run(aId, bId, vcfFile, annoTab, confTab, outDir, outputPrefix, verbose):
             confidenceScore = None
             confDF[['Chr1', 'Chr2']] = confDF[['Chr1', 'Chr2']].astype(str)
             indexList = confDF.loc[confDF['Chr1'].isin([chrom1]) &
-                                   confDF['Start1'].isin([int(start1 - ciPosNeg)]) &
+                                   confDF['Start1'].isin([int(start1 - abs(ciPosNeg))]) &
                                    confDF['Chr2'].isin([chrom2]) &
-                                   confDF['Start2'].isin([int(start2 - ciEndNeg)])].index.tolist()
+                                   confDF['Start2'].isin([int(start2 - abs(ciEndNeg))])].index.tolist()
 
             if(len(indexList) > 1):
                 if(verbose):
