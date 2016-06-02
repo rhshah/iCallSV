54748ab2dcdbffd1b16dcacd271709d141ea6249
Ronak H Shah <rons.shah@gmail.com>
2016-06-02 10:47:45 -0400

    Bug Fixes

--- iCallSV/checkHotSpotList.py
+++ iCallSV/checkHotSpotList.py
@@ -51,7 +51,7 @@ def CheckIfItIsHotspot(chr1, start1, chr2, start2, hotspotDict):
                     (hsStart, hsEnd, gene) = re.split(pattern, item)
                     if((start >= int(hsStart) and start <= int(hsEnd)) or (end >= int(hsStart) and end <= int(hsEnd))):
                         hotspotTag = True
-                        break
+                        return(hotspotTag)
                     else:
                         hotspotTag = False
             else:
@@ -59,6 +59,7 @@ def CheckIfItIsHotspot(chr1, start1, chr2, start2, hotspotDict):
                 (hsStart, hsEnd, gene) = re.split(pattern, allCordinates)
                 if((start >= int(hsStart) and start <= int(hsEnd)) or (end >= int(hsStart) and end <= int(hsEnd))):
                     hotspotTag = True
+                    return(hotspotTag)
                 else:
                     hotspotTag = False
         else:
@@ -75,7 +76,7 @@ def CheckIfItIsHotspot(chr1, start1, chr2, start2, hotspotDict):
                     (hsStart, hsEnd, gene) = re.split(pattern, item)
                     if(start1 >= int(hsStart) and start1 <= int(hsEnd)):
                         hotspotTag = True
-                        break
+                        return(hotspotTag)
                     else:
                         hotspotTag = False
             else:
@@ -83,6 +84,7 @@ def CheckIfItIsHotspot(chr1, start1, chr2, start2, hotspotDict):
                 (hsStart, hsEnd, gene) = re.split(pattern, allCordinates)
                 if(start1 >= int(hsStart) and start1 <= int(hsEnd)):
                     hotspotTag = True
+                    return(hotspotTag)
                 else:
                     hotspotTag = False
         if ((chr2 in hotspotDict) and (hotspotTag is False)):
@@ -96,7 +98,7 @@ def CheckIfItIsHotspot(chr1, start1, chr2, start2, hotspotDict):
                     (hsStart, hsEnd, gene) = re.split(pattern, item)
                     if(start2 >= int(hsStart) and start2 <= int(hsEnd)):
                         hotspotTag = True
-                        break
+                        return(hotspotTag)
                     else:
                         hotspotTag = False
             else:
@@ -104,9 +106,10 @@ def CheckIfItIsHotspot(chr1, start1, chr2, start2, hotspotDict):
                 (hsStart, hsEnd, gene) = re.split(pattern, allCordinates)
                 if(start2 >= int(hsStart) and start2 <= int(hsEnd)):
                     hotspotTag = True
+                    return(hotspotTag)
                 else:
                     hotspotTag = False
-    return(hotspotTag)
+    
 # # Test module
 # hotspotDict = ReadHotSpotFile(
 #     "/home/shahr2/workspace/dmp-data/mskdata/interval-lists/structuralvariants_geneInterval.txt")
