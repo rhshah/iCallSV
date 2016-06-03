e9dcd915994211d03cd273d97e011b4c8a4543c0
Ronak H Shah <rons.shah@gmail.com>
2016-06-03 17:58:36 -0400

    Updated Docs

--- doc/conf.py
+++ doc/conf.py
@@ -28,9 +28,11 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-sys.path.insert(0,os.path.abspath('..'))
+sys.path.insert(0,os.path.abspath('/Users/shahr2/git/iCallSV/iCallSV'))
 extensions = [
 	'sphinx.ext.autodoc',
+	'sphinx.ext.todo',
+	'sphinx.ext.coverage',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
